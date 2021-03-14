3fbb214da3d522636b2de59bb60b7fb089c6a256
MartinPetrakiev <MartinPetrakiev@users.noreply.github.com>
2021-03-15 01:01:20 +0200

    hash password

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,10 +1,20 @@
+import bcrypt from 'bcrypt';
 export default {
     Query: {
         getUser: (parent, { id }, { models }) => models.user.findOne({ where: { id } }),
         allUsers: (parent, args, { models }) => models.user.findAll(),
     },
     Mutation: {
-        createUser: (parent, args, { models }) => models.user.create(args)
-    }
+        register: async (parent, { password, ...otherArgs }, { models }) => {
+            try {
+                const hash = bcrypt.hashSync(password, 10);
+                const user = await models.user.create({ ...otherArgs, password: hash });
+                return true;
+            } catch (err) {
+                console.log(err);
+                return false;
+            }
+        },
+    },
 
 };
