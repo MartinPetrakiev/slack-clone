5280a9f30c4570d920c91b8670c77a7f6538a8b1
MartinPetrakiev <MartinPetrakiev@users.noreply.github.com>
2021-03-15 01:34:13 +0200

    fix server connection

--- src/index.js
+++ src/index.js
@@ -4,10 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import {
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache
+} from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:8080/graphql',
+  cache: new InMemoryCache()
+});
+
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
 
--- src/routes/Register.js
+++ src/routes/Register.js
@@ -2,27 +2,29 @@ import React, { useState } from 'react';
 import { Button, Container, Form, Header, Input } from 'semantic-ui-react';
 import { useMutation, gql } from '@apollo/client';
 
-const registerMutation = gql`
+const REGISTER_MUTATION = gql`
     mutation($username: String!, $email: String!, $password: String!) {
-        register(username: $username, email: $email, password: $password)
+        register(username: $username, email: $email, password: $password) {
+            token
+        }
       }
 `;
 
 function Register() {
-    const [register, { data, error }] = useMutation(registerMutation);
+    const [register] = useMutation(REGISTER_MUTATION);
     const user = {
         username: '',
         email: '',
         password: ''
     };
 
-    const [userState, setUserState] = useState(user);
-    const { username, email, password } = userState;
+    const [formState, setformState] = useState(user);
+    const { username, email, password } = formState;
 
     const onSubmit = async (e) => {
         e.preventDefault();
         try {
-            const res = await register({ variables: userState });
+            const res = await register({ variables: formState });
             console.log(res);
         } catch (error) {
             console.log(error);
@@ -31,7 +33,7 @@ function Register() {
 
     const handleChange = (e) => {
         const { name, value } = e.target;
-        setUserState(state => ({ ...state, [name]: value }));
+        setformState(state => ({ ...state, [name]: value }));
     };
 
     return (
