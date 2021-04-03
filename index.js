480eeaa6eb61a984019436370153807d86679fd7
MartinPetrakiev <MartinPetrakiev@users.noreply.github.com>
2021-04-03 12:42:40 +0300

    setup websocket authentication

--- index.js
+++ index.js
@@ -59,7 +59,32 @@ const models = sequelize.models;
 const server = new ApolloServer({
   schema,
   subscriptions: {
-    path: '/subscriptions'
+    path: '/subscriptions',
+    onConnect: async (connectionParams, webSocket) => {
+      if (token && refreshToken) {
+        let user = null;
+        try {
+          const payload = jwt.verify(token, SECRET);
+          user = payload.user;
+        } catch (err) {
+          const newTokens = await refreshTokens(token, refreshToken, models, SECRET, SECRET2);
+          user = newTokens.user;
+        }
+        if (!user) {
+          throw new Error('Invalid auth tokens');
+        }
+
+        const member = await models.member.findOne({ where: { teamId: 1, userId: user.id } });
+
+        if (!member) {
+          throw new Error('Missing auth tokens!');
+        }
+
+        return true;
+      }
+
+      throw new Error('Missing auth tokens!');
+    },
   },
   context: ({ req, connection }) => ({
     models,
